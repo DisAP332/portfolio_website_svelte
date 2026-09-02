456c34de0ae0cd2cfffc51031c8ef2c82c4dd682
Jordan Bell <113725255+DisAP332@users.noreply.github.com>
2026-09-02 16:42:09 -0400

    Update professional contact email

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -17,14 +17,7 @@
 	<div class="lottieMobile">
 		<a href="https://github.com/DisAP332" target="_blank">
 			{#if LottiePlayer}
-				<LottiePlayer
-					src={gitHubLottie}
-					autoplay={true}
-					loop={true}
-					renderer="svg"
-					background="transparent"
-					style="height: 8vh; width: 8vh;"
-				/>
+				<LottiePlayer src={gitHubLottie} autoplay={true} loop={true} renderer="svg" background="transparent" style="height: 8vh; width: 8vh;" />
 			{/if}
 		</a>
 	</div>
@@ -36,181 +29,38 @@
 	<div class="lottieMobile">
 		<a href="https://www.linkedin.com/in/jordan-bell-135205251/" target="_blank">
 			{#if LottiePlayer}
-				<LottiePlayer
-					src={linkedInLottie}
-					autoplay={true}
-					loop={true}
-					renderer="svg"
-					background="transparent"
-					style="height: 8vh; width: 8vh;"
-				/>
+				<LottiePlayer src={linkedInLottie} autoplay={true} loop={true} renderer="svg" background="transparent" style="height: 8vh; width: 8vh;" />
 			{/if}
 		</a>
 	</div>
 	<div class="lottieDesktop">
 		<a href="https://github.com/DisAP332" target="_blank">
 			{#if LottiePlayer}
-				<LottiePlayer
-					src={gitHubLottie}
-					autoplay={true}
-					loop={true}
-					renderer="svg"
-					background="transparent"
-					style="height: 7.5vh; width: 7.5vh;"
-				/>
+				<LottiePlayer src={gitHubLottie} autoplay={true} loop={true} renderer="svg" background="transparent" style="height: 7.5vh; width: 7.5vh;" />
 			{/if}
 		</a>
 		<a href="https://www.linkedin.com/in/jordan-bell-135205251/" target="_blank">
 			{#if LottiePlayer}
-				<LottiePlayer
-					src={linkedInLottie}
-					autoplay={true}
-					loop={true}
-					renderer="svg"
-					background="transparent"
-					style="height: 7.5vh; width: 7.5vh;"
-				/>
+				<LottiePlayer src={linkedInLottie} autoplay={true} loop={true} renderer="svg" background="transparent" style="height: 7.5vh; width: 7.5vh;" />
 			{/if}
 		</a>
-		<a href="mailto: disap332@gmail.com">
-			<button class="hireMeButton">Hire me?</button>
-		</a>
-		<a href={resume} download="Jordan_Bell_Resume">
-			<img src={resumeIcon} alt="resume" class="logo mt-4" />
-		</a>
+		<a href="mailto:jordan.l.bell1998@gmail.com"><button class="hireMeButton">Contact me</button></a>
+		<a href={resume} download="Jordan_Bell_Resume"><img src={resumeIcon} alt="resume" class="logo mt-4" /></a>
 	</div>
 </nav>
 
 <style lang="postcss">
-	nav {
-		@apply lg:grid grid-cols-2 shadow-xl !important;
-		background-color: teal;
-		height: 8.5vh;
-		display: flex;
-		justify-content: space-evenly;
-		width: 100vw;
-		border-bottom: 3px solid rgb(0, 92, 92);
-	}
-	nav > div {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: row;
-		height: 100%;
-	}
-	/* ------name------- */
-	.nameFont1 {
-		font-family: 'nameFont1';
-		font-size: 4.5vh;
-	}
-	.nameFont2 {
-		font-family: 'nameFont2';
-		font-size: 4.8vh;
-		color: var(--BellRed);
-	}
-	.nameContainer {
-		@apply mt-2;
-	}
-	/* ------lottie------ */
-	.lottieMobile {
-		@apply lg:hidden;
-	}
-	.lottieDesktop {
-		@apply hidden lg:flex;
-	}
-	/* ------logo------- */
-	.logo {
-		@apply lg:block !important;
-		display: none;
-		height: 7vh;
-		transform: translate(0.8vh, -1vh);
-	}
-	/* -------hire me button ------- */
-
-	.hireMeButton {
-		@apply ml-10;
-		width: 10vw;
-		height: 5.5vh;
-		box-shadow: 0px 0px 0px 0px black;
-		border: 2px solid black;
-		border-radius: 30px;
-		font-size: 1vw;
-		font-family: nameFont2;
-		color: white;
-		background: linear-gradient(
-			27deg,
-			rgba(236, 10, 87, 1) 30%,
-			rgba(68, 157, 47, 0.7511379551820728) 46%,
-			rgba(81, 79, 219, 0.8995973389355743) 60%,
-			rgba(210, 79, 219, 0.8995973389355743) 80%
-		);
-		padding: 0.6em 2em;
-		border: 2px solid white;
-		outline: none;
-		color: rgb(255, 255, 255);
-		background: #111;
-		cursor: pointer;
-		position: relative;
-		z-index: 0;
-		border-radius: 10px;
-		user-select: none;
-		-webkit-user-select: none;
-		touch-action: manipulation;
-	}
-
-	.hireMeButton:before {
-		content: '';
-		background: linear-gradient(
-			45deg,
-			#ff0000,
-			#ff7300,
-			#fffb00,
-			#48ff00,
-			#00ffd5,
-			#002bff,
-			#7a00ff,
-			#ff00c8,
-			#ff0000
-		);
-		position: absolute;
-		top: -2px;
-		left: -2px;
-		background-size: 400%;
-		z-index: -1;
-		filter: blur(5px);
-		-webkit-filter: blur(5px);
-		width: calc(100% + 4px);
-		height: calc(100% + 4px);
-		animation: glowing-button-85 20s linear infinite;
-		transition: opacity 0.3s ease-in-out;
-		border-radius: 10px;
-	}
-
-	@keyframes glowing-button-85 {
-		0% {
-			background-position: 0 0;
-		}
-		50% {
-			background-position: 400% 0;
-		}
-		100% {
-			background-position: 0 0;
-		}
-	}
-
-	.hireMeButton:after {
-		z-index: -1;
-		content: '';
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		background: #222;
-		left: 0;
-		top: 0;
-		border-radius: 10px;
-	}
-
-	.hireMeButton:hover {
-		box-shadow: 0px 0px 49px 3px rgba(0, 225, 255, 1);
-	}
+	nav { @apply lg:grid grid-cols-2 shadow-xl !important; background-color: teal; height: 8.5vh; display: flex; justify-content: space-evenly; width: 100vw; border-bottom: 3px solid rgb(0, 92, 92); }
+	nav > div { display: flex; justify-content: center; align-items: center; flex-direction: row; height: 100%; }
+	.nameFont1 { font-family: 'nameFont1'; font-size: 4.5vh; }
+	.nameFont2 { font-family: 'nameFont2'; font-size: 4.8vh; color: var(--BellRed); }
+	.nameContainer { @apply mt-2; }
+	.lottieMobile { @apply lg:hidden; }
+	.lottieDesktop { @apply hidden lg:flex; }
+	.logo { @apply lg:block !important; display: none; height: 7vh; transform: translate(0.8vh, -1vh); }
+	.hireMeButton { @apply ml-10; width: 10vw; min-width: 120px; height: 5.5vh; border: 2px solid white; border-radius: 10px; font-size: clamp(.75rem, 1vw, 1rem); font-family: nameFont2; color: white; background: #111; cursor: pointer; position: relative; z-index: 0; user-select: none; }
+	.hireMeButton:before { content: ''; background: linear-gradient(45deg,#ff0000,#ff7300,#fffb00,#48ff00,#00ffd5,#002bff,#7a00ff,#ff00c8,#ff0000); position: absolute; top: -2px; left: -2px; background-size: 400%; z-index: -1; filter: blur(5px); width: calc(100% + 4px); height: calc(100% + 4px); animation: glowing-button-85 20s linear infinite; border-radius: 10px; }
+	@keyframes glowing-button-85 { 0% { background-position: 0 0; } 50% { background-position: 400% 0; } 100% { background-position: 0 0; } }
+	.hireMeButton:after { z-index: -1; content: ''; position: absolute; width: 100%; height: 100%; background: #222; left: 0; top: 0; border-radius: 10px; }
+	.hireMeButton:hover { box-shadow: 0 0 49px 3px rgba(0,225,255,1); }
 </style>
