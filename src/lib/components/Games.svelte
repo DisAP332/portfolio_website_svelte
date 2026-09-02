cc1a6b68daf3ddd86e46d901ff47c62c8642bad2
Jordan Bell <113725255+DisAP332@users.noreply.github.com>
2026-09-02 17:31:51 -0400

    Turn game section into playable showcase

--- src/lib/components/Games.svelte
+++ src/lib/components/Games.svelte
@@ -4,42 +4,32 @@
 	let GamesData: Array<object> = GamesDataJSON;
 </script>
 
-<div class="transparentBackground mt-20" id="games">
+<section class="transparentBackground mt-20 gameSection" id="games">
 	<div class="headingCenter largeMiddle largeStyling">
-		<div>
-			<div class="horizontalLine" />
-		</div>
-		<div>
-			<h1 class="coolFont">Games</h1>
-		</div>
-		<div>
-			<div class="horizontalLine" />
-		</div>
+		<div><div class="horizontalLine" /></div>
+		<div><h1 class="coolFont">Games</h1></div>
+		<div><div class="horizontalLine" /></div>
 	</div>
+
 	<div class="subHead mt-1">
 		<div class="visabilityHelper">
-			<p>Most not mobile friendly. May revisit in the future.</p>
+			<p>Small projects from when I was learning. You can play them here and look through the original code.</p>
 		</div>
 	</div>
-	<div class="gamesContainer visabilityHelper">
+
+	<div class="gamesContainer">
 		{#if GamesData}
 			{#each GamesData as GameData}
 				<Game {GameData} />
 			{/each}
 		{/if}
 	</div>
-</div>
+</section>
 
 <style lang="postcss">
-	.gamesContainer {
-		@apply flex justify-center mt-3;
-		flex-wrap: wrap;
-	}
-	h1 {
-		@apply text-5xl lg:text-6xl;
-		color: white;
-	}
-	.largeStyling {
-		@apply lg:pt-1 pb-2;
-	}
+	.gameSection { @apply px-3 lg:px-6 pt-4 pb-14; }
+	.gamesContainer { @apply flex flex-col items-center gap-8 mt-8; }
+	h1 { @apply text-5xl lg:text-6xl; color: white; }
+	.largeStyling { @apply lg:pt-1 pb-2; }
+	.subHead p { font-family: softFont; }
 </style>
