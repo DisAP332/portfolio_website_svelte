f1e3682eba97b896ef5b9210c2098124b2add5bd
Jordan Bell <113725255+DisAP332@users.noreply.github.com>
2026-09-02 16:42:18 -0400

    Update footer positioning

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,16 +3,14 @@
 </script>
 
 <footer>
-	<p>Jordan Bell Web Dev @{dateYear}</p>
+	<div>
+		<p>Jordan Bell • Technology, diagnostics & software • {dateYear}</p>
+		<a href="mailto:jordan.l.bell1998@gmail.com">jordan.l.bell1998@gmail.com</a>
+	</div>
 </footer>
 
-<style>
-	footer {
-		@apply grid-cols-2 shadow-xl;
-		background-color: teal;
-		height: 8.5vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+<style lang="postcss">
+	footer { @apply shadow-xl; background-color: teal; min-height: 8.5vh; display: flex; justify-content: center; align-items: center; padding: 1rem; text-align: center; color: white; border-top: 3px solid rgb(0, 92, 92); }
+	footer div { @apply flex flex-col gap-1; font-family: softFont; }
+	footer a { color: white; text-decoration: underline; text-underline-offset: 3px; }
 </style>
